d50a5b5621f5a5d12c547c158679b9297b29bc96
austinfast <65324947+austinfast@users.noreply.github.com>
2022-01-11 22:24:48 -0700

    Update lgeku_scraper.py

--- lgeku_scraper.py
+++ lgeku_scraper.py
@@ -6,5 +6,5 @@ class LGEKUScraper(KubraScraper):
     repo = "power-outage-data"
     filepath = "lgeku/outages.json"
 
-    instance_id = "877fd1e9-4162-473f-b782-d8a53a85326b" #"7636a60f-7b81-4fb0-a30d-ed79a8e271e7" #
-    view_id = "a6cee9e4-312b-4b77-9913-2ae371eb860d" #"3471c92-6e1b-494b-a884-391139a2cc18" #
+    instance_id = "7636a60f-7b81-4fb0-a30d-ed79a8e271e7" #"877fd1e9-4162-473f-b782-d8a53a85326b" #
+    view_id = "3471c92-6e1b-494b-a884-391139a2cc18" #"a6cee9e4-312b-4b77-9913-2ae371eb860d" #
